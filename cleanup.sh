d655aa2e84a060a11018616b1465a48cf3c6c409
Larry Smith Jr <mrlesmithjr@gmail.com>
2015-12-30 16:02:06 -0500

    Updated cleanup script
    Signed-off-by: Larry Smith Jr <mrlesmithjr@gmail.com>

--- cleanup.sh
+++ cleanup.sh
@@ -3,3 +3,11 @@ vagrant destroy -f
 rm -rf .vagrant
 rm host_vars/*
 touch host_vars/dummy
+docker-machine rm mh-keystore -f
+docker-machine rm mhs-demo0 -f
+docker-machine rm mhs-demo1 -f
+docker-machine rm mhs-demo2 -f
+docker-machine rm kv -f
+docker-machine rm c0-master -f
+docker-machine rm c0-n1 -f
+docker-machine rm c0-n2 -f
